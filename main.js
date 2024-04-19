23fff56ac28a1ac6cb826d9276cf15da107fd2f2
CQuarkH <currihuile@gmail.com>
2024-04-19 16:13:34 -0400

    Unbound fixed

--- main.js
+++ main.js
@@ -33,38 +33,48 @@ const startGame = (speed) => {
    const fightAreaHeight = fightArea.offsetHeight;
 
    controlPlayer1(speed, fightAreaHeight, fightAreaWidth);
-   controlPlayer2(speed);
+   controlPlayer2(speed, fightAreaHeight, fightAreaWidth);
 };
 
 const controlPlayer1 = (speed, height, width) => {
    let box = document.getElementById("player1");
 
    document.addEventListener("keydown", (event) => {
-      if (box.offsetWidth + parseInt(box.style.left || 0) + 10 < width) {
-         if (event.key === "w") {
-            box.style.top = `${parseInt(box.style.top || 0) - speed}px`;
-         } else if (event.key === "s") {
-            box.style.top = `${parseInt(box.style.top || 0) + speed}px`;
-         } else if (event.key === "a") {
-            box.style.left = `${parseInt(box.style.left || 0) - speed}px`;
-         } else if (event.key === "d") {
-            box.style.left = `${parseInt(box.style.left || 0) + speed}px`;
-         }
+      if (event.key === "w" && parseInt(box.style.top || 0) - speed >= 0) {
+         box.style.top = `${parseInt(box.style.top || 0) - speed}px`;
+      } else if (
+         event.key === "s" &&
+         parseInt(box.style.top || 0) + box.offsetHeight + speed <= height
+      ) {
+         box.style.top = `${parseInt(box.style.top || 0) + speed}px`;
+      } else if (event.key === "a" && parseInt(box.style.left || 0) - speed >= 0) {
+         box.style.left = `${parseInt(box.style.left || 0) - speed}px`;
+      } else if (
+         event.key === "d" &&
+         parseInt(box.style.left || 0) + box.offsetWidth + speed <= width
+      ) {
+         box.style.left = `${parseInt(box.style.left || 0) + speed}px`;
       }
    });
 };
 
-const controlPlayer2 = (speed) => {
+const controlPlayer2 = (speed, height, width) => {
    let box = document.getElementById("player2");
 
    document.addEventListener("keydown", (event) => {
-      if (event.key === "ArrowUp") {
+      if (event.key === "ArrowUp" && parseInt(box.style.top || 0) - speed >= 0) {
          box.style.top = `${parseInt(box.style.top || 0) - speed}px`;
-      } else if (event.key === "ArrowDown") {
+      } else if (
+         event.key === "ArrowDown" &&
+         parseInt(box.style.top || 0) + box.offsetHeight + speed <= height
+      ) {
          box.style.top = `${parseInt(box.style.top || 0) + speed}px`;
-      } else if (event.key === "ArrowLeft") {
+      } else if (event.key === "ArrowLeft" && parseInt(box.style.left || 0) - speed >= 0) {
          box.style.left = `${parseInt(box.style.left || 0) - speed}px`;
-      } else if (event.key === "ArrowRight") {
+      } else if (
+         event.key === "ArrowRight" &&
+         parseInt(box.style.left || 0) + box.offsetWidth + speed <= width
+      ) {
          box.style.left = `${parseInt(box.style.left || 0) + speed}px`;
       }
    });
